bd5d39a0f17c622749cafb3bbbcd6edc6af2044e
sakumanaomi <naomi011298@gmail.com>
2022-06-11 12:21:38 +0900

    css

--- kadai1_create.php
+++ kadai1_create.php
@@ -1,20 +1,37 @@
 <?php
 if(
   !isset($_POST['name']) || $_POST['name'] == '' ||
-  !isset($_POST['mailaddress']) || $_POST['mailaddress'] == ''
+  !isset($_POST['mailaddress']) || $_POST['mailaddress'] == '' ||
   !isset($_POST['age']) || $_POST['age'] == '' ||
   !isset($_POST['gender']) || $_POST['gender'] == '' ||
   !isset($_POST['inquiry']) || $_POST['inquiry'] == ''
 ){
-  exit(paramError'')
+  exit('paramError');
 }
 
 $name = $_POST['name'];
-$mailaddress = $_POST['meiladdress'];
+$mailaddress = $_POST['mailaddress'];
 $age = $_POST['age'];
 $gender = $_POST['gender'];
 $inquiry = $_POST['inquiry'];
 
 include('functions.php');
 $pdo = connect_to_db();
+$sql = 'INSERT INTO inquiry_table(id, name, mailaddress, age, gender, inquiry, created_at, updated_at) VALUES(NULL, :name,:mailaddress, :age, :gender, :inquiry, now(),now())';
 
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+$stmt->bindValue(':mailaddress', $mailaddress, PDO::PARAM_STR);
+$stmt->bindValue(':age', $age, PDO::PARAM_STR);
+$stmt->bindValue(':gender', $gender, PDO::PARAM_STR);
+$stmt->bindValue(':inquiry', $inquiry, PDO::PARAM_STR);
+
+try {
+  $status = $stmt->execute();
+} catch (PDOException $e) {
+  echo json_encode(["sql error" => "{$e->getMessage()}"]);
+  exit();
+}
+
+header("Location:kadai1_input.php");
+exit();
--- kadai1_input.php
+++ kadai1_input.php
@@ -5,26 +5,34 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>課題１</title>
+   <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
   <form action="kadai1_create.php" method="POST">
     <fieldset>
       <legend>お問い合わせ内容</legend>
-      <div>
-        氏名: <input type="text" name="name">
+      <div class="form_item">
+        <label for=name class="form_item_label">氏名:</label>
+        <input type="text" class="form_item_input" name="name">
+      </div>
+      <div class="form_item">
+        <label for="mailaddress" class="form_item_label">メールアドレス:</label>  
+        <input type="text" class="form_item_input" name="mailaddress">
+      </div>
+      <div class="form_item">
+        <label for="age" class="form_item_label">年齢:</label>  
+        <input type="text" class="form_item_input" name="age">
+      </div>
+      <div class="form_item">
+        <label for="gender" class="form_item_label">性別:</label>
+        <input type="radio" class="form_item_input_radio" name="gender" value="male">男性
+        <input type="radio" class="form_item_input_radio" name="gender" value="female">女性
+      </div>
+      <div class="form_item">
+        お問い合わせ内容: <textarea rows="10" cols="60" class="form_item_textarea" name="inquiry"></textarea>
       </div>
       <div>
-        メールアドレス:<input type="text" name="mailaddress">
-      </div>
-      <div>
-        年齢:<input type="text" name="age">
-      </div>
-      <div>
-        性別:<input type="radio" name="gender" value="male">男性
-        <input type="radio" name="gender" value="female">女性
-      </div>
-      <div>
-        お問い合わせ内容: <input type="text" name="inquiry">
+        <button>送信する</button>
       </div>
     </fieldset>
   </form>
